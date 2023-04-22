c3c1f96e15300ea054dc48cfcb47122870aaf649
JayCabasag99 <jay@metromart.com>
2023-04-22 15:39:54 +0800

    Adde fixes

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import { useState } from 'react'
-import { Project } from '../utils/types'
+import { Project } from '@/src/utils/types'
 
 interface ProjectCardProps {
   project: Project
